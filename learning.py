da1f43a6a714a129ef1c4455a8799401b5ddf4a1
100148965 <canterojessie@gmail.com>
2019-09-30 15:52:33 -0500

    learned some maaaaaaaaaaaaathhhhh

--- learning.py
+++ learning.py
@@ -5,3 +5,12 @@ print("what are we doing?")
 print("printing...yay!!")
 print("my head hurts")
 print("how do you make websites on here, is that even possible")
+
+#practice with math
+print("I will now count my cats:")
+print("cat --->", 100 - 25 * 3 % 4)
+# % = moduluce operate, take two numbers, divides them idk takes the quotents TAKES A 4TH MAYBE idk
+print("now i will count my dogs:")
+print("dog --->", 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+print("is it true that 3 + 2 < 5 - 7")
+print(3 + 2 < 5 - 7)
