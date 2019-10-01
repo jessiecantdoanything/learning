439c025ff3cbb1d337a0dbeea14707cf97623dcc
100148965 <canterojessie@gmail.com>
2019-10-01 15:52:43 -0500

    put explanations for each piece of code

--- learning.py
+++ learning.py
@@ -1,16 +1,25 @@
 # <-- this is an octothorpe B)
+# I first start the code with a message that introduces everything
 print("hello world")
+# here it puts a message about western sayings to continue introducing
 print("howdy, yonder!")
-print("what are we doing?")
+# this puts the final message about what it's doing on the screen and excitment.
 print("printing...yay!!")
-print("my head hurts")
-print("how do you make websites on here, is that even possible")
 
-#practice with math
+
+# practice with math
+# (starting a new sentence on the screen about counting cats)
 print("I will now count my cats:")
+# this does the math problem that counts our "cats"
 print("cat --->", 100 - 25 * 3 % 4)
+
 # % = moduluce operate, take two numbers, divides them idk takes the quotents TAKES A 4TH MAYBE idk
+# this is to start up a new message saying our next task
 print("now i will count my dogs:")
+# this does a math equation that counts the dogs
 print("dog --->", 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+# this is now doing a problem where it asks us whether an equation is true or false
 print("is it true that 3 + 2 < 5 - 7")
+# this is the statement with the math equation telling us whether it is true or false
 print(3 + 2 < 5 - 7)
+
