453117fbe49341634f5615915d53d6ed96f17999
100148965 <canterojessie@gmail.com>
2019-10-02 15:43:56 -0500

    learning variable w mr black B)

--- learning.py
+++ learning.py
@@ -11,13 +11,13 @@ print("printing...yay!!")
 # (starting a new sentence on the screen about counting cats)
 print("I will now count my cats:")
 # this does the math problem that counts our "cats"
-print("cat --->", 100 - 25 * 3 % 4)
+print("cats --->", 100 - 25 * 3 % 4)
 
 # % = moduluce operate, take two numbers, divides them idk takes the quotents TAKES A 4TH MAYBE idk
 # this is to start up a new message saying our next task
 print("now i will count my dogs:")
 # this does a math equation that counts the dogs
-print("dog --->", 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+print("dogs --->", 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
 # this is now doing a problem where it asks us whether an equation is true or false
 print("is it true that 3 + 2 < 5 - 7")
 # this is the statement with the math equation telling us whether it is true or false
@@ -27,3 +27,20 @@ print(3 + 2 < 5 - 7)
 print("now i will solve another math problem")
 # this is the math equation
 print(4 + 6 * 9 - 2.2 )
+
+# variables
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = cars
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+
+print("there are",cars,"cars available.")
+print("there are only",drivers,"drivers available.")
+print("there will be",cars_not_driven,"empty cars today.")
+print("we can transport",carpool_capacity,"people today.")
+print("we have",passengers,"to carpool today.")
+print("we need to put about",average_passengers_per_car,"in each car.")
